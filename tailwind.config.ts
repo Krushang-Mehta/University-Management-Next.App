372288e9a707bf66e1b0a619bceb7a3de0690a4b
Krushang-Mehta <krushang2005@gmail.com>
2025-07-15 20:05:52 +0530

    Initial Ui Commponents

---
+++ tailwind.config.ts
@@ -0,0 +1,34 @@
+import type { Config } from "tailwindcss";
+
+
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors:{
+        sky: "#C3EBFA",
+        skylight: "#EDF9FD",
+        purple: "#CFCEFF",
+        purpleLight: "#F1F0FF",
+        yellow: "#FAE27C",
+        yellowLight: "#FEFCE8",
+        candy: "#feafc7",
+        candylight: "#feccdc",
+        cyan: "#53eafd",
+        cyanlight: "#a2f4fd",
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
