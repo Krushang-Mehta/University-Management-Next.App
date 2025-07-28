b1da940aeb17d8af4544671959dd194fb26599ee
Krushang-Mehta <krushang2005@gmail.com>
2025-07-28 10:30:04 +0530

    List  table Components

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -15,7 +15,7 @@ const Table = ({
             <thead>
                 <tr className="text-left text-gray-500 text-sm">
                     {columns.map(col => (
-                        <th key={col.accessor}>
+                        <th key={col.accessor} className={col.className}>
                             {col.header}
                         </th>
                     ))}
